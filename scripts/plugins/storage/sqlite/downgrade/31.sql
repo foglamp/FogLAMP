5b1aed65b160991282ea2ffacf9e94035a4dce9e
Stefano Simonelli <stefano@dianomic.com>
2020-01-09 14:16:29 +0000

    FOGL-3604: fixed UUID Generation
    Signed-off-by: Stefano Simonelli <stefano@dianomic.com>

--- scripts/plugins/storage/sqlite/downgrade/31.sql
+++ scripts/plugins/storage/sqlite/downgrade/31.sql
@@ -17,7 +17,7 @@ INSERT INTO new_readings
         id,
         asset_code,
         -- UUID Generation
-        (select hex( randomblob(4)) || '-' || hex( randomblob(2)) || '-' || '4' || substr( hex( randomblob(2)), 2) || '-' || substr('AB89', 1 + (abs(random()) % 4) , 1)  || substr(hex(randomblob(2)), 2) || '-' || hex(randomblob(6)) ),
+        (select lower(hex( randomblob(4))) || '-' || lower(hex( randomblob(2))) || '-' || '4' || substr( lower(hex( randomblob(2))), 2) || '-' || substr('89ab', 1 + (abs(random()) % 4) , 1)  || substr(lower(hex(randomblob(2))), 2) || '-' || lower(hex(randomblob(6)) )),
         reading,
         user_ts,
         ts
