ce862b0fa32e806b258f297c4ae9476ff0ce1400
Stefano Simonelli <stefano@dianomic.com>
2020-01-10 08:36:12 +0000

    FOGL-3604: PG upgrade/downgrade
    Signed-off-by: Stefano Simonelli <stefano@dianomic.com>

---
+++ scripts/plugins/storage/postgres/upgrade/32.sql
@@ -0,0 +1,41 @@
+--
+-- DROP the column: read_key   uuid
+--
+
+ALTER TABLE readings DROP CONSTRAINT readings_pkey;
+
+CREATE TABLE foglamp.new_readings (
+    id         bigint                      NOT NULL DEFAULT nextval('foglamp.readings_id_seq'::regclass),
+    asset_code character varying(50)       NOT NULL,                      -- The provided asset code. Not necessarily located in the
+                                                                          -- assets table.
+    reading    jsonb                       NOT NULL DEFAULT '{}'::jsonb,  -- The json object received
+    user_ts    timestamp(6) with time zone NOT NULL DEFAULT now(),        -- The user timestamp extracted by the received message
+    ts         timestamp(6) with time zone NOT NULL DEFAULT now(),
+    CONSTRAINT readings_pkey PRIMARY KEY (id) );
+
+
+INSERT INTO new_readings
+SELECT
+    id,
+    asset_code,
+    reading,
+    user_ts,
+    ts
+FROM readings;
+
+DROP INDEX fki_readings_fk1;
+DROP INDEX readings_ix2;
+DROP INDEX readings_ix3;
+
+DROP TABLE readings;
+
+ALTER TABLE new_readings rename to readings;
+
+CREATE INDEX fki_readings_fk1
+    ON foglamp.readings USING btree (asset_code, user_ts desc);
+
+CREATE INDEX readings_ix2
+    ON foglamp.readings USING btree (asset_code);
+
+CREATE INDEX readings_ix3
+    ON foglamp.readings USING btree (user_ts);
