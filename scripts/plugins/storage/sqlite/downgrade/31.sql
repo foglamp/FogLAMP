b6ef8b0de356385314924969a059e2b20adf372b
Stefano <stefano@dianomic.com>
2020-01-14 08:55:38 +0000

    FOGL-3604: SQLite/PG downgrade fixed.
    Signed-off-by: Stefano <stefano@dianomic.com>

--- scripts/plugins/storage/sqlite/downgrade/31.sql
+++ scripts/plugins/storage/sqlite/downgrade/31.sql
@@ -17,7 +17,7 @@ INSERT INTO new_readings
         id,
         asset_code,
         -- UUID Generation
-        (select lower(hex( randomblob(4))) || '-' || lower(hex( randomblob(2))) || '-' || '4' || substr( lower(hex( randomblob(2))), 2) || '-' || substr('89ab', 1 + (abs(random()) % 4) , 1)  || substr(lower(hex(randomblob(2))), 2) || '-' || lower(hex(randomblob(6)) )),
+        (lower(hex( randomblob(4))) || '-' || lower(hex( randomblob(2))) || '-' || '4' || substr( lower(hex( randomblob(2))), 2) || '-' || substr('89ab', 1 + (abs(random()) % 4) , 1)  || substr(lower(hex(randomblob(2))), 2) || '-' || lower(hex(randomblob(6)) )) as read_key,
         reading,
         user_ts,
         ts
